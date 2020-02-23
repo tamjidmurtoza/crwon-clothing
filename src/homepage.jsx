b799a7c87c9c157711f0d9e0b1e5091f9b149da7
tamjidmurtoza <tamjid.murtoza@gmail.com>
2020-02-23 20:27:55 +0600

    update

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -12,19 +12,19 @@ const HomePage = () => {
         </div>
         <div className="menu-item">
           <div className="content">
-            <h2 className="title">HATS</h2>
+            <h2 className="title">HATS1</h2>
             <div className="subtitle">Shop Now</div>
           </div>
         </div>
         <div className="menu-item">
           <div className="content">
-            <h2 className="title">HATS</h2>
+            <h2 className="title">HATS2</h2>
             <div className="subtitle">Shop Now</div>
           </div>
         </div>
         <div className="menu-item">
           <div className="content">
-            <h2 className="title">HATS</h2>
+            <h2 className="title">HATS3</h2>
             <div className="subtitle">Shop Now</div>
           </div>
         </div>
